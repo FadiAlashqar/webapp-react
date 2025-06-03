0d6da44293f052f8754f6b8b4ce597c06ee8979a
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-06-04 01:24:17 +0200

    [UPD] milestone 2 done

--- src/assets/pages/Details.jsx
+++ src/assets/pages/Details.jsx
@@ -1,70 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
-const initialFilms = [
-    {
-        id: 1,
-        title: "Titolo 1",
-        author: "Regista 1",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 2,
-        title: "Titolo 2",
-        author: "Regista 2",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 3,
-        title: "Titolo 3",
-        author: "Regista 3",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 4,
-        title: "Titolo 4",
-        author: "Regista 4",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 5,
-        title: "Titolo 5",
-        author: "Regista 5",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 6,
-        title: "Titolo 6",
-        author: "Regista 6",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    }
-
-];
 
 const Details = () => {
     const { id } = useParams();
-    const [films, setFilms] = useState(initialFilms);
     const [film, setFilm] = useState({});
 
-    const fetchFilm = (() => {
-        films.forEach((actualFilm) => {
-            if (actualFilm.id === parseInt(id)) {
-                setFilm(actualFilm);
-            }
+    const getFilms = () => {
+        axios.get(`http://127.0.0.1:3000/api/movie/${id}`).then((resp) => {
+            setFilm(resp.data);
         })
-    })
+    }
 
     useEffect(() => {
-        fetchFilm();
+        getFilms();
     }, [])
+
+
 
     return (
         <div className="container">
@@ -75,13 +30,24 @@ const Details = () => {
             </div>
             <div className="row">
                 <div className="col-12 col-md-6 col-lg-4" key={`actualFilm${film.id}`}>
-                    <img className='img-fluid' src={film.image} alt="" />
+                    <img className='img-fluid' src={`/public/imgs/movies_cover/${film.image}`} alt="" />
                 </div>
                 <div className="col-12 col-md-6 col-lg-8">
                     <h2>{film.title}</h2>
-                    <h3><em>{film.author}</em></h3>
+                    <h3><em>{film.director}</em></h3>
                     <p>{film.abstract}</p>
                 </div>
+            </div>
+            <div className="row gy-4 mt-4">
+                {film.reviews?.map((review) => {
+                    return <div key={`review ${review.id}`} className="col-12">
+                        <div className="card p-4">
+                            <p>{review.name}</p>
+                            <p>{review.vote}</p>
+                            <p>{review.text}</p>
+                        </div>
+                    </div>
+                })}
             </div>
         </div>
     )
